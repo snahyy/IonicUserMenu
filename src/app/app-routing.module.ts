295337f51470863dda976c6802341a2462766cb1
snahyy <108914151+snahyy@users.noreply.github.com>
2023-06-19 21:12:08 -0300

    Solucionando problema de versionamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,18 +6,15 @@ const routes: Routes = [
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
   { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule) },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardPageModule) },
-  { path: 'agenda', loadChildren: () => import('./agenda/agenda.module').then(m => m.AgendaPageModule) },  {
-<<<<<<< Updated upstream
+  { path: 'agenda', loadChildren: () => import('./agenda/agenda.module').then(m => m.AgendaPageModule) },
+  {
     path: 'paciente',
     loadChildren: () => import('./paciente/paciente.module').then( m => m.PacientePageModule)
   },
   {
     path: 'funcionario',
     loadChildren: () => import('./funcionario/funcionario.module').then( m => m.FuncionarioPageModule)
-=======
-    path: 'funcionario-cadastro',
-    loadChildren: () => import('./funcionario/funcionario-cadastro/funcionario-cadastro.module').then( m => m.FuncionarioCadastroPageModule)
->>>>>>> Stashed changes
+
   },
 
 ];
