045894384cc2e11fe00fd2ff41a26bc62bf82776
snahyy <108914151+snahyy@users.noreply.github.com>
2023-06-19 23:31:46 -0300

    ai calica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    HTTP
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/core/service/http.service.ts
+++ src/app/core/service/http.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HTTP, HTTPResponse } from '@ionic-native/http/ngx';  
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HTTP) { }
+
+ async get<T>(url: String){
+    const result = await this.http.get(url);
+    return this.readResult<T>(result);
+  }
+
+  private readResult<T>(result: HttpResponse){
+    if (result && result.data){
+      return JSON.parse(result.data) as T;
+    }
+    
+    return null;
+  }
 }
